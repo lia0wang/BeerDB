dfa770488e4d8e2ab351fa709dc8cc9c5dc583e3
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-10-14 00:08:27 +1100

    done question1-9

--- ass1.sql
+++ ass1.sql
@@ -9,66 +9,116 @@
 
 
 -- Q1: oldest brewery
+-- earliest founded
 
-create or replace view Q1(brewery)
-as
-...
+CREATE OR REPLACE VIEW Q1(brewery)
+AS
+SELECT name
+FROM breweries
+WHERE founded = (SELECT MIN(founded) FROM breweries)
 ;
 
 -- Q2: collaboration beers
+-- beers wich has > 1 brewery, defined by their id
 
-create or replace view Q2(beer)
-as
-...
+CREATE OR REPLACE VIEW Q2(beer)
+AS
+SELECT name
+FROM beers
+JOIN brewed_by ON beers.id = brewed_by.beer
+GROUP BY id
+HAVING COUNT(brewery) > 1
 ;
 
 -- Q3: worst beer
+-- lowest rating
 
-create or replace view Q3(worst)
-as
-...
+CREATE OR REPLACE VIEW Q3(worst)
+AS
+SELECT name
+FROM beers
+WHERE rating = (SELECT MIN(rating) FROM beers)
 ;
 
 -- Q4: too strong beer
+-- beers whose ABV is higher than the maximum ABV for their style
 
-create or replace view Q4(beer,abv,style,max_abv))
-as
-...
+CREATE OR REPLACE VIEW Q4(beer,abv,style,max_abv)
+AS
+SELECT beers.name, beers.abv, styles.name, styles.max_abv
+FROM beers
+JOIN styles ON beers.style = styles.id
+WHERE beers.abv > styles.max_abv
 ;
 
 -- Q5: most common style
+-- determined by the number of beers brewed to that style
 
-create or replace view Q5(style)
-as
-...
+CREATE OR REPLACE VIEW V1(name, number)
+AS
+SELECT styles.name, COUNT(*)
+FROM styles
+JOIN beers ON beers.style = styles.id
+GROUP BY styles.name
+;
+
+CREATE OR REPLACE VIEW Q5(style)
+AS
+SELECT name
+FROM V1
+WHERE NUMBER = (SELECT MAX(number) FROM V1)
 ;
 
 -- Q6: duplicated style names
+-- differ only in the upper/lower case of their letters
+-- the lexicographically smaller style name should be in style1.
 
-create or replace view Q6(style1,style2)
-as
-...
+CREATE OR REPLACE VIEW Q6(style1,style2)
+AS
+SELECT A.name AS style1, B.name AS style2
+FROM styles A, styles B
+WHERE A.name < B.name
+AND LOWER(A.name) = LOWER(B.name)
 ;
 
 -- Q7: breweries that make no beers
 
-create or replace view Q7(brewery)
-as
-...
+CREATE OR REPLACE VIEW Q7(brewery)
+AS
+SELECT name AS brewery
+FROM breweries
+WHERE id NOT IN (SELECT brewery FROM brewed_by)
 ;
 
 -- Q8: city with the most breweries
 
-create or replace view Q8(city,country)
-as
-...
+CREATE OR REPLACE VIEW V2(id,number)
+AS
+SELECT locations.id, COUNT(locations.id)
+FROM locations
+JOIN breweries ON locations.id = breweries.located_in
+WHERE locations.metro IS NOT NULL
+GROUP BY locations.id;
+
+CREATE OR REPLACE VIEW Q8(city,country)
+AS
+SELECT locations.metro AS city, locations.country
+FROM locations
+JOIN v2 ON locations.id = v2.id
+WHERE v2.number = (SELECT max(v2.number) FROM v2)
 ;
 
 -- Q9: breweries that make more than 5 styles
 
-create or replace view Q9(brewery,nstyles)
-as
-...
+CREATE OR REPLACE VIEW Q9(brewery,nstyles)
+AS
+SELECT breweries.name AS brewery, COUNT(DISTINCT styles.id) AS nstyles
+FROM brewed_by
+JOIN breweries ON brewed_by.brewery = breweries.id
+JOIN beers ON brewed_by.beer = beers.id
+JOIN styles ON beers.style = styles.id
+GROUP BY breweries.name
+HAVING COUNT(DISTINCT styles.id) > 5
 ;
 
 -- Q10: beers of a certain style
